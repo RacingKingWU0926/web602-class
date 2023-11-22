e0499f3f105d3c9a506d6c4b7fd568656c6691c8
JasonLei <jlei@activision.com>
2023-11-21 20:34:21 -0800

    added group

---
+++ week5-day2/group/vue.js
@@ -0,0 +1,43 @@
+Vue.component('card', {
+    props: ['info', 'title'],
+    template: `
+        <div class="card">
+            <ul class="card-info card-shadow">
+                <li class="card-header">{{ title }}</li>
+                <li v-for="(value, label) in info.details" class="card-item">
+                    <span>{{ value }}</span> {{ label }}
+                </li>
+                <li class="card-item">
+                    <h2 class="card-wide">{{ info.price }}</h2>
+                    <span class="card-opacity">{{ info.priceUnit }}</span>
+                </li>
+                <li class="card-btn">
+                    <button class="button">Sign Up</button>
+                </li>
+            </ul>
+        </div>
+    `
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        premiumInfo: {
+            title: 'Premium',
+            details: {
+                Storage: '50GB',
+                Emails: 50,
+                Domains: 50,
+                Support: 'Endless'
+            },
+            price: '$ 50',
+            priceUnit: 'per month'
+        },
+    },
+    computed: {
+        membershipTitle: function() {
+            // dynamically change the title if it were "Premium".
+            return this.premiumInfo.title === 'Premium' ? 'GOLD' : this.premiumInfo.title;
+        }
+    },
+});
